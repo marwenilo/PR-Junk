2747d2444e8877b1412e2480fd338fc427074d20
Marwen Magri <marwen.magri@gomycode.co>
2020-04-03 20:39:15 +0100

    start off welc page

--- src/App.js
+++ src/App.js
@@ -1,34 +1,23 @@
-import React from 'react';
-import welcomIdea from  './assets/welcom-idea.jpg';
-import './App.css';
-  
+import React from "react";
+
+import "./App.css";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <div className="headerContainer">
-       
-       
-       
-     
-     
-       </div>
-      <div className="bodyContainer"> 
-      
-      
-      
-      
-      
-      <h1>Hello There</h1>
-
-
-<p> My name is <strong>Marwen Magri</strong> and in this site i'm building right now you will find a lot of useful react UI 
-components and alote of <strong>"JavaScript-React-Redux"</strong> code Junk</p>
-
-
-
-      </div>
-      
+        <div className="headerContainer"></div>
+        <div className="bodyContainer">
+          <h1>Hello There</h1>
+          <p>
+            {" "}
+            My name is
+            <strong>Marwen Magri</strong> and in this site i'm building right
+            now you will find a lot of useful react UI components and many of
+            <strong>"JavaScript-React-Redux"</strong>
+            code Junk
+          </p>
+        </div>
       </header>
     </div>
   );
