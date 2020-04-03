8cf7d0654c805c7396cec3923eb738b7d3d4acdf
Marwen Magri <marwen.magri@gomycode.co>
2020-04-03 22:15:48 +0100

    prep dep code editor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
-
+// import ReactMarkdown from "react-markdown"
 import "./App.css";
-
+// https://www.npmjs.com/package/react-simple-code-editor
+// https://www.npmjs.com/package/jshint
 function App() {
+  const input = `
+  {
+    showUsers && (
+      <ul>
+        {users.map(user => <li>{user.name}</li>)}
+      </ul>
+    )}
+    `
   return (
     <div className="App">
       <header className="App-header">
@@ -12,11 +21,15 @@ function App() {
           <p>
             {" "}
             My name is
-            <strong>Marwen Magri</strong> and in this site i'm building right
+            <strong> Marwen Magri</strong> and in this site i'm building right
             now you will find a lot of useful react UI components and many of
-            <strong>"JavaScript-React-Redux"</strong>
-            code Junk
+            <strong> "JavaScript-React-Redux" </strong>
+            code Junk.
           </p>
+          <div>
+  {/*<p>if you only return one side of the conditional rendering anyway, is using the && operator:</p>*/}
+          
+          </div>
         </div>
       </header>
     </div>
