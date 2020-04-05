60358bb92b41865a1b4761ba6f6e715e10ef026b
Marwen Magri <marwen.magri@gomycode.co>
2020-04-05 17:13:35 +0100

    js destruction

---
+++ src/junk/javascript/Destructuring .js
@@ -0,0 +1,35 @@
+const student = {
+    ID: '21',
+    name: 'Jhon',
+    GPA: '3.0',
+  };
+  
+  const id = student.ID;
+  const name = student.name;
+  const GPA = student.GPA;
+
+  //or
+
+  const {ID, name, GPA} = student;
+
+
+  // no destructuring
+const users = this.state.users;
+const counter = this.state.counter;
+
+// destructuring
+const { users, counter } = this.state;
+
+//Another great feature is the rest destructuring. 
+//It is often used for splitting out a part of an object, 
+//but keeping the remaining properties in another object.
+// rest destructuring
+const { users, ...rest } = this.state;
+
+
+
+
+
+  console.log(id);
+  console.log(name);
+  console.log(GPA);
